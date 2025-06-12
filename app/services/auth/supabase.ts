c6d722f46de004e20280a9fe8d7f70fd445a0633
Ricardo Ma <106693821+riricardoMa@users.noreply.github.com>
2025-06-12 10:40:58 -0700

    fix: build

--- app/services/auth/supabase.ts
+++ app/services/auth/supabase.ts
@@ -1,14 +1,16 @@
+import "react-native-url-polyfill/auto";
 import { createClient } from "@supabase/supabase-js";
 import { AppState } from "react-native";
-import * as SessionStorage from "@utils/storage/SessionStorage";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
-    storage: SessionStorage,
+    storage: AsyncStorage,
     autoRefreshToken: true,
+    persistSession: true,
     detectSessionInUrl: false,
   },
 });
