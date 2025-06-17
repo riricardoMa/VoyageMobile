218351a936803c8cd36873bc0aa76d40e648fee0
Ricardo Ma <106693821+riricardoMa@users.noreply.github.com>
2025-06-16 19:04:59 -0700

    feat: add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,26 @@ module.exports = {
   ],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Custom brand colors from Fuschia and Iris palette
+        fuschia: {
+          'rodeo-dust': '#c3b39d',    // rgb(195, 179, 157)
+          'vanilla': '#d5c8b8',       // rgb(213, 200, 184)
+          'dust-storm': '#e1d6ca',    // rgb(225, 214, 202)
+        },
+        iris: {
+          'parchment': '#f1e5da',     // rgb(241, 229, 218)
+          'bone': '#e3d1c6',          // rgb(227, 209, 198)
+        },
+        // You can also use these as semantic color names
+        primary: '#c3b39d',           // Fuschia Rodeo Dust
+        secondary: '#d5c8b8',         // Fuschia vanilla
+        accent: '#e1d6ca',            // Fuschia Dust Storm
+        background: '#f1e5da',        // Iris parchment
+        surface: '#e3d1c6',           // Iris Bone
+      },
+    },
   },
   plugins: [],
 }; 
